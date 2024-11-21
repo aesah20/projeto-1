e9dfd1cb8e3dcf836d9c2e9a64c391e6d0d4d370
aesah20 <sara.reis.oliveira@escola.pr.gov.br>
2024-11-21 11:42:04 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+  background("purple");
+}
+
+function draw() {
+  rect(mouseX, mouseY, 200, 700); //divide a tela
+  fill("rgb(9,103,135)");
+  stroke("rgb(213,72,180)0,219,249");
+  if(mouseIsPressed)
+rect(mouseX, mouseY, 200, 700); //divide a tela
